9472dee57f6189e0c853004bb3566d8144b0bcbd
Dintle25 <dintlemili25@gmail.com>
2026-05-28 03:42:02 +0200

    update the Program.cs to Configure global Problem Details error handling

--- API/Program.cs
+++ API/Program.cs
@@ -4,7 +4,15 @@ using Scalar.AspNetCore;
 var Builder = WebApplication.CreateBuilder(args);
 
 //Registering the services
-Builder.Services.AddControllers();   //registering builder support
+Builder.Services.AddControllers()
+.AddJsonOptions(options =>
+    {
+      
+        options.JsonSerializerOptions.Converters.Add(
+            new System.Text.Json.Serialization.JsonStringEnumConverter());
+    });   //registering builder support
+
+Builder.Services.AddProblemDetails();     // Turns all errors into standard format
 
 Builder.Services.AddOpenApi();     //registering built-in OpenAPI document
 
@@ -18,6 +26,12 @@ if(app.Environment.IsDevelopment())
     app.MapOpenApi();
     app.MapScalarApiReference();
 }
+
+//Error Handling
+app.UseExceptionHandler();     // Catches unexpected errors and shows them nicely
+app.UseStatusCodePages();      // Turns 404, 400 and other status codes into nice error messages
+
+app.UseHttpsRedirection();
 
 app.MapControllers();
 app.Run();
