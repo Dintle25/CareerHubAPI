6ef2256d697c8a9cad0299efe4b2320da007f4c6
Dintle25 <dintlemili25@gmail.com>
2026-06-08 19:36:52 +0200

    add local changes

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea335c21921fefdd60956ccb9588daf046be1efe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f90923bb29d0098c97f32b39cc5ea200a7644d45")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
