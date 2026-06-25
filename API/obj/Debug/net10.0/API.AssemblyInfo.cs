a6967ed0e54513a476433657250883fb04cc89aa
Dintle25 <dintlemili25@gmail.com>
2026-06-25 03:09:24 +0200

    update api

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6759bb6351d484fddc3fbf838bf8b353c3680ab2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+151cdd6600cb2818789c390988755b3044d251c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
