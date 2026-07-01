8347a04ea8bf4fdcde8e397c2de9065edf55bd86
Dintle25 <dintlemili25@gmail.com>
2026-07-01 02:36:05 +0200

    update to use the snakbar and dialogs

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d307823a5ba4304aa0d4ed9ea3b4c89383bbe310")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41cfd32dd6c3825598204e58199a511d81b44179")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
